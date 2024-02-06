6a76b34fc3d4476e544bff04f7af4a725a703647
RejanBR <156968905+RejanBR@users.noreply.github.com>
2024-02-06 19:23:43 -0300

    Entrega semana 3

---
+++ atividadeparaentrega/untitled/src/Main.java
@@ -0,0 +1,39 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Seja bem vindo ao COMEX!");
+
+        int dataNascimentoFulano = 1980;
+        int dataNascimentoSicrano = 2000;
+        System.out.println("_____________________________");
+        System.out.println("Listagem dos CLientes");
+        System.out.println("Nome: fulano");
+        System.out.println("Data de Nascimento: =  " + dataNascimentoFulano);
+        System.out.println("_____________________________");
+        System.out.println("Nome: Sicrano");
+        System.out.println("Data de Nascimento: =  " + dataNascimentoSicrano);
+
+
+
+        double precolivro = 49.95;
+        double precoCelular = 1470.76;
+        System.out.println(">>> Listagem de Produtos");
+        System.out.println("_____________________________");
+        System.out.println("Nome: Livro");
+        System.out.println("Preço= R$ 49.95");
+        System.out.println("_____________________________");
+        System.out.println("Nome: Celular");
+        System.out.println("Preço= R$ 1470.76");
+
+
+
+
+
+
+
+
+    }
+}
